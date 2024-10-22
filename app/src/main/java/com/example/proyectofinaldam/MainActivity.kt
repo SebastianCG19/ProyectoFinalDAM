5101e5c86b94869f3821d1d6cc81497a8ff0f5be
SebastianCG19 <116407682+SebastianCG19@users.noreply.github.com>
2024-10-21 21:10:20 -0500

    coommit

--- app/src/main/java/com/example/proyectofinaldam/MainActivity.kt
+++ app/src/main/java/com/example/proyectofinaldam/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.proyectofinaldam.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
